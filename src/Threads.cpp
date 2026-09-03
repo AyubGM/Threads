b170d8781d035b38f7fdf879c6bfebfba3598fed
AyubGM <ayubgmdev@gmail.com>
2026-09-03 04:09:27 +0200

    multiple

--- src/Threads.cpp
+++ src/Threads.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <thread>
-
-
-
+#include <vector>
 
 int main()
 {
 	auto Test = [](int x) {
-		std::cout << "Hello from thread! x = " << x << "\n";
+		std::cout << "Hello from thread! = " << std::this_thread::get_id() << "\n";
+		std::cout << "Arg = " << x << "\n";
 		};
-	std::thread myThread(Test, 1);
 
+
+	std::vector<std::thread> threads;
+
+	for (int i = 0; i < 10; ++i) {
+		threads.emplace_back(Test, i + 1);
+	}
 
     std::cout << "Hello main!\n";
-    myThread.join();
+
+	for (auto& t : threads) {
+		t.join();
+	}
 
 }
