a452d369b4fe80636aa1632ed7490e933e461b3a
AyubGM <ayubgmdev@gmail.com>
2026-09-03 03:51:28 +0200

    Introduction

--- src/Threads.cpp
+++ src/Threads.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <thread>
+
+void Test(int x) {
+	std::cout << "Hello from thread! x = " << x << std::endl;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::thread myThread(Test, 42);
+
+    std::cout << "Hello main!\n";
+    myThread.join();
 }
