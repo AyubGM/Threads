2d569295821e73e0d57dcdf278361c511cde9c61
AyubGM <ayubgmdev@gmail.com>
2026-09-03 05:52:49 +0200

    mutex

--- src/Threads.cpp
+++ src/Threads.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <mutex>
+
+std::mutex g_Mutex;
+static int g_counter = 0;
+
+void IncrementCounter()
+{
+	g_Mutex.lock();
+	g_counter = g_counter + 1;
+  g_Mutex.unlock();
+}
 
 int main()
 {
-	auto Test = [](int x) {
-		std::cout << "Hello from thread! = " << std::this_thread::get_id() << "\n";
-		std::cout << "Arg = " << x << "\n";
-		};
-
-
 	std::vector<std::jthread> threads;
 
 	for (int i = 0; i < 1000; ++i) {
-		threads.emplace_back(Test, i + 1);
+		threads.emplace_back(IncrementCounter);
 	}
 
-    std::cout << "Hello main!\n";
+	threads.clear();
+
+    std::cout << "Final counter value: " << g_counter << "\n";
 
 
 }
